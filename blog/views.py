5f53c1bb23c07dd0fb64b5ce603274d12a201631
Damian Piotrowski <damian.p199@gmail.com>
2026-01-20 21:47:11 +0000

    add default bootstrap to templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'ASD123',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'ASD321',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
 def blog_home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def blog_about(request):
     return render(request, 'blog/about.html')
