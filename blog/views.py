6b53283cb5f42c33e45355a1a1e324146da45322
Damian Piotrowski <damian.p199@gmail.com>
2026-01-23 18:59:08 +0000

    small cleanup, initial db structure in models, navigation tab links

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts = [
-    {
-        'author': 'ASD123',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'ASD321',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
-
 def blog_home(request):
     context = {
         'posts': posts
