fe76f5e9c2d2eeb6f3f4c67f2218e1e261700ae4
Damian Piotrowski <damian.p199@gmail.com>
2026-02-24 17:47:13 +0100

    stylesheets and js + small changes

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def blog_home(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/home.html', context)
+
+    return render(request, 'blog/home.html')
 
 def blog_about(request):
     return render(request, 'blog/about.html')
